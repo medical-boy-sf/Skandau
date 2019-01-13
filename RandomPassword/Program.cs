353be5d27814ac576a31259097d495ca1cdff8a0
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-14 01:36:01 +0200

    RandomPassword.cs

---
+++ RandomPassword/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RandomPassword
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rd = new Random();
+            string password = String.Empty;
+
+            for (int i = 0; i < 15; i++)
+            {
+                int randNum = rd.Next(0, 9);
+                password += randNum;
+            }
+
+            Console.WriteLine(password);
+        }
+    }
+}
