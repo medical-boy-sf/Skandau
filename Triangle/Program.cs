8641dc72622865df5626fea33df4406480af23fc
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-14 02:12:46 +0200

    Triangle.cs

---
+++ Triangle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Triangle
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(new string(' ', n - i) + new string('*', 2 * i - 1));
+            }
+        }
+    }
+}
