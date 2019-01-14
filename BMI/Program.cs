9f818b13208d365efca413f668253eb486d8e8d4
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-14 09:36:38 +0200

    BMI

---
+++ BMI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BMI
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            double weight = Double.Parse(Console.ReadLine());
+            double height = Double.Parse(Console.ReadLine());
+            double bmi = weight / (height * height);
+            Console.WriteLine($"Your BMI is {bmi:F2}");
+
+            if (bmi >= 25)
+            {
+                Console.WriteLine("Overweight!");
+            }
+            
+            else if (bmi > 18)
+            {
+                Console.WriteLine($"Normal weight.");
+            }
+
+            else
+            {
+                Console.WriteLine("Underweight! Eat more! ");
+            }
+        }
+    }
+}
