c133d7a2e78164683ee9804f9ef77c10d989e33c
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-14 09:07:03 +0200

    AgeCategories

---
+++ AgeCategories/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AgeCategories
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 18)
+            {
+                Console.WriteLine("You are an adult! You can enter!");
+            }
+            
+            else if (age >= 14)
+            {
+                Console.WriteLine("Underaged! You need to turn 18 to enter!");
+            }
+
+            else if (age >= 0)
+            {
+                Console.WriteLine("Minor! You cannot enter!");
+            }
+
+            else
+            {
+                Console.WriteLine("You've entered invalid age!");
+            }
+        }
+    }
+}
